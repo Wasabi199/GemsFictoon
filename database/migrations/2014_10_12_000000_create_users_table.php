736d3b0122a0786059f88c4d1d43629848f73e47
Wasabi199 <xxryan0909@gmail.com>
2024-02-28 02:38:48 +0800

    mysql error

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->integer('usertype')->index()->comment(UserType::class);
-            $table->longText('biography')->default('The user has no bio yet.')->nullable();
+            $table->longText('biography')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
